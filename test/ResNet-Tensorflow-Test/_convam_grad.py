ffd16bf6e5e3f840addc5b6d89e596a827ac0e17
Jing Gong <ausaaronyl@gmail.com>
2021-07-17 19:02:47 +1000

    added profile test

---
+++ test/ResNet-Tensorflow-Test/_convam_grad.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import sparse_ops
+convam_grad_module = tf.load_op_library('./convam_gpu.so')
+
+@ops.RegisterGradient("Convam")
+def _convam_grad_cc(op,grad):
+  """The derivatives for deconvolution.
+
+  Args:
+    op: the Deconvolution op.
+    grad: the tensor representing the gradient w.r.t. the output
+
+  Returns:
+    the gradients w.r.t. the input and the filter (should be done in one gradient moudle)
+  """
+  dilations = op.get_attr("dilations")
+  strides = op.get_attr("strides")
+  padding = op.get_attr("padding")
+  data_format = op.get_attr("data_format")
+  shape_0, shape_1 = array_ops.shape_n([op.inputs[0], op.inputs[1]])
+  return  [convam_grad_module.convam_input_grad(shape_0,op.inputs[1],grad,
+          dilations=dilations,
+          strides=strides,
+          padding=padding,
+          data_format=data_format),convam_grad_module.convam_filter_grad(op.inputs[0],shape_1,grad,
+          dilations=dilations,
+          strides=strides,
+          padding=padding,
+          data_format=data_format)]
