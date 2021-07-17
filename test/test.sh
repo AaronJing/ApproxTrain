f939fef70051b5f1b4e8fd4b0f07ef74c40de13b
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-07-17 15:17:39 +1000

    fixed the test script

--- test/test.sh
+++ test/test.sh
@@ -17,15 +17,15 @@ nvcc -O2 -I cuda/ -rdc=true -c cuda/FPmultMBM_fast32.cu -o test/FPmultMBM_fast32
 nvcc -O2 -I cuda/ -rdc=true -c cuda/FPmultMBM_fast16.cu -o test/FPmultMBM_fast16.o
 
 echo "Accurate multiply "
-nvcc -O2 -dlink -I cuda/ test/test_gemm.cu test/gemm.o  -o test/gemm_ac.out
+nvcc -O2 -I cuda/ test/test_gemm.cu test/gemm.o  -o test/gemm_ac.out
 test/gemm_ac.out > test/data/gemm_ac.txt && diff -q test/data/ac_ref_32bit.txt.exp test/data/gemm_ac.txt || die "Accurate multiply failed"
 
 echo ""
 echo "FMBM 32 multiply failed"
-nvcc -O2 -dlink -I cuda/ -DFMBM32_MULTIPLIER=1 test/test_gemm.cu test/gemm.o  test/FPmultMBM_fast32.o -o test/gemm_am32.out
+nvcc -O2 -I cuda/ -DFMBM32_MULTIPLIER=1 test/test_gemm.cu test/gemm.o  test/FPmultMBM_fast32.o -o test/gemm_am32.out
 test/test/gemm_am32.out > test/data/gemm_am32.txt && diff -q test/data/am_ref_32bit.txt.exp test/data/gemm_am32.txt|| die "FMBM 32 multiply failed"
 
 echo ""
 echo "FMBM 16 multiply failed"
-nvcc -O2 -dlink -I cuda/ -DFMBM16_MULTIPLIER=1 test/test_gemm.cu test/gemm.o cuda/FPmultMBM_fast16.cu -o test/gemm_am16.out
+nvcc -O2 -I cuda/ -DFMBM16_MULTIPLIER=1 test/test_gemm.cu test/gemm.o cuda/FPmultMBM_fast16.cu -o test/gemm_am16.out
 test/gemm_am16.out > test/data/gemm_am16.txt && diff -q test/data/am_ref_16bit.txt.exp test/data/gemm_am16.txt|| die "FMBM 16 multiply failed"
