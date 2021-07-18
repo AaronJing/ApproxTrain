cb8d303f69e385ded2fbfd5df66d9230905786e2
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-07-18 12:17:29 +1000

    more tests

--- test/test.sh
+++ test/test.sh
@@ -15,13 +15,13 @@ rm -f test/*.out
 
 echo "Accurate multiply "
 nvcc -O2 -I cuda/ test/test_gemm.cu cuda/gemm.cu  -o test/gemm_ac.out
-test/gemm_ac.out > test/data/gemm_ac.txt && diff -q test/data/ac_ref_32bit.txt.exp test/data/gemm_ac.txt || die "Accurate multiply failed"
+test/gemm_ac.out > test/data/gemm_ac.txt && diff -q -w test/data/ac_ref_32bit.txt.exp test/data/gemm_ac.txt || die "Accurate multiply failed"
 
 echo ""
-for i in 32 16
+for i in 32 16 14 12 10
 do
 	echo "FMBM $i multiply"
 	nvcc -O2 -I cuda/ -DFMBM${i}_MULTIPLIER=1 test/test_gemm.cu cuda/gemm.cu  -o test/gemm_am${i}.out
-	test/gemm_am${i}.out > test/data/gemm_am${i}.txt && diff -q test/data/am_ref_${i}bit.txt.exp test/data/gemm_am${i}.txt|| die "FMBM $i multiply failed"
+	test/gemm_am${i}.out > test/data/gemm_am${i}.txt && diff -q -w test/data/am_ref_${i}bit.txt.exp test/data/gemm_am${i}.txt|| die "FMBM $i multiply failed"
 	echo ""
 done
