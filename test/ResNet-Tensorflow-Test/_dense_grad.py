df6a2e4623742905a8dcff39d8381358660d1906
Jing Gong <ausaaronyl@gmail.com>
2021-12-21 23:06:26 +1100

    restructured

--- test/ResNet-Tensorflow-Test/_dense_grad.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.framework import ops
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import sparse_ops
-dense_grad_module = tf.load_op_library('cuda_op_kernel.so')
-
-@ops.RegisterGradient("Dense")
-def _dense_grad_cc(op, grad):
-    return dense_grad_module.dense_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
