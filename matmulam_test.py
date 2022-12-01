cfc39af25ed929d6677e6efd815748070c3308d9
AaronJing <ausaaronyl@gmail.com>
2022-12-01 17:38:14 +1100

    update

--- matmulam_test.py
+++ matmulam_test.py
@@ -1,12 +1,12 @@
 
 import tensorflow as tf
-from python.ops.math_ops import matmulam
+#from python.ops.math_ops import matmulam
 tf.debugging.set_log_device_placement(True)
 
-a = tf.constant([1.2, 2, 3, 4, 5, 6], shape=[3, 2])
-b = tf.constant([7.3, 8, 9, 10, 11, 12], shape=[3, 2])
-c = matmulam(a, b, True)
-print(c)
+a = tf.constant([1, 2, 3, 4, 5, 6], shape=[3, 1, 2])
+b = tf.constant([7, 8, 9, 10], shape=[2, 2, 1])
+#c = matmulam(a, b, True)
+#print(c)
 d = tf.linalg.matmul(a, b, True)
 print(d)
 #print(matmulam)
